e6a8a89c42d6cda2e5e667a980f0b9e27a1bdd80
unknown <hokaji0689@gmail.com>
2024-10-27 01:45:23 +0100

    feat:display all products:images,names and prices after fetching

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 document.addEventListener('DOMContentLoaded',async function() {
     getProducts();
-    
+    var products  = await getProducts();
+    displayProducts(products);
 });
 
 async function getProducts() {
-    const res = await fetch("https://fake-coffee-api.vercel.app/api?limit=5");
+    const res = await fetch("https://fake-coffee-api.vercel.app/api");
     products = await res.json();
     return products;
 }
+
+function createProductCard(product) {
+    const card = document.createElement('div');
+    card.classList.add('product-card');
+
+    title= document.createElement("h1");
+    title.textContent = `${product.name}`;
+    image=document.createElement("img");
+    image.src=`${product.image_url}`;
+    price=document.createElement("h2");
+    price.textContent=`${product.price} Dhs`;
+    
+
+    card.appendChild(image);
+    card.appendChild(title);
+    card.appendChild(price);
+   
+   
+    return card;
+}
+
+
+async function displayProducts(products) {
+    var product_content = document.getElementById("product_content"); 
+    products.forEach(product => {
+        var card = createProductCard(product);
+        product_content.appendChild(card);
+    });
+}
+
+
+
