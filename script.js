0bef82209797ffadc79e0e8858bc1b5cee0db5df
unknown <hokaji0689@gmail.com>
2024-11-20 11:56:39 +0100

    feat:add indexdb data base

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 
 async function run() {
     var p = await getProducts();
-    displayProducts(p);
+    displayProducts(p);    
+    addProductsToDB(p);
+    
+
+    
+    
+    
+   
 }
 run();
 
 async function getProducts() {
-    const res = await fetch("https://fake-coffee-api.vercel.app/api");
-    products = await res.json();
-    return products;
+    try {
+        const res = await fetch("https://fake-coffee-api.vercel.app/api");
+        products = await res.json();
+        return products;
+    }
+    catch(error) {
+        console.error("Erreur lors de la récupération des produits:",error);
+        loadProductsFromDB();
+       };
+    
+    
 }
 
 function createProductCard(product) {
@@ -27,6 +42,7 @@ function createProductCard(product) {
     add_to_cart_btn = document.createElement("button");
     add_to_cart_btn.textContent ="Add To Cart";
     add_to_cart_btn.classList.add('atc_button');
+    add_to_cart_btn.addEventListener("click",()=>{addToCart(product.id)});
 
     card.appendChild(image);
     card.appendChild(title);
@@ -72,11 +88,12 @@ async function filterProducts(){
 let product_content = document.getElementById("product_content");  
 let listView=document.getElementById("list");
 let gridView=document.getElementById("grid");
-let product_card=document.querySelector(".product-card");
+let product_card=document.querySelector(".product-card")
 
 function setGridView(){
-    product_content.style.display="grid"
-
+    product_content.style.display="grid";
+    product_card.style.width="100%";
+    
 
 
     
@@ -91,3 +108,73 @@ function clearBtn(){
     btn.style.visibility="hidden"
     var input = document.getElementById("search-input").value="";
 }
+
+function openDB() {
+    let request = indexedDB.open("CoffeeShopDB", 1);
+   
+    request.onupgradeneeded =  () => {
+        let res = request.result;
+        res.createObjectStore("products", { keyPath: "id" });
+        console.log("upgrade done");
+    }
+    return request;
+}
+
+function addProductsToDB(products){
+    let request = openDB();
+    request.onsuccess = () => {
+        let res = request.result;
+        let tx = res.transaction("products","readwrite");
+        let store = tx.objectStore("products");
+        products.forEach(product => {
+            store.add(product);
+        });
+        console.log("success");
+        
+    }
+} 
+
+
+function loadProductsFromDB(){
+    let request = openDB();
+    
+    request.onsuccess = () => {
+        let res=request.result;
+        let tx = res.transaction("products","readwrite");
+        let store = tx.objectStore("products");
+        newres=store.getAll()
+        newres.onsuccess = () =>{
+            products = newres.result;
+            displayProducts(products);
+            console.log("load products succes")
+        }
+    }
+}
+
+async function  addToCart(productId){
+    let products = await getProducts(); 
+    let product_detail= products.find(p => p.id == productId);
+    console.log(product_detail);
+    let cartItem = {
+        id: productId,
+        image_url: product_detail.image_url,
+        name:product_detail.name,
+        price: product_detail.price,
+        quantity: 1
+    };
+    console.log(cartItem);
+    let request = openDB();
+    
+    request.onsuccess = () => {
+        let res=request.result;
+        let tx = res.transaction("cart","readwrite");
+        let store = tx.objectStore("cart");
+        store.add(cartItem);
+        console.log(`produit  ${cartItem.id} ajoutee a la carte avec succes`);
+        
+    }
+
+}
+
+
+
